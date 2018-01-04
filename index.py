5b083a5b4c09823f60eb8fb4fbfe76a99d165216
Andrew Chellis <andrewchellis@users.noreply.github.com>
2018-01-04 20:23:03 +0900

    Add api instead of scraping

--- index.py
+++ index.py
@@ -5,19 +5,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
-baseUrl = "https://weather.com/weather/today/l/{}:4:US"
+baseScrapeUrl = "https://weather.com/weather/today/l/{}:4:US"
+darkSkyKey = ""
+try:
+    with open("darkSkyKey.txt") as darkSkyKeyFile:
+        darkSkyKey = darkSkyKeyFile.read()
+except IOError as e: 
+    raise Exception("Must supply an api key under darkSkyKey.txt") from e
+except:
+    raise Exception("Something terrible happened")
 
-
-
-def getWeatherInfo(zipcode):
-    if type(zipcode)==str and len(zipcode)==5 and zipcode.isdigit():
-        requestUrl = baseUrl.format(zipcode)
-        weather = requests.get(requestUrl)
-        if weather.status_code==200:
-            pageData = BeautifulSoup(weather.text, 'html.parser')
-            temperature = pageData.find(class_="today_nowcard-temp").span.get_text().replace("°","")
-            # data = pageData.find(id="APP").find_all('div')[0]
-            print(temperature)
+def getWeatherInfo(zipcode, shouldScrape=False):
+    validZip = type(zipcode)==str and len(zipcode)==5 and zipcode.isdigit()
+    if validZip:
+        if shouldScrape:
+            requestUrl = baseScrapeUrl.format(zipcode)
+            weather = requests.get(requestUrl)
+            if weather.status_code==200:
+                pageData = BeautifulSoup(weather.text, 'html.parser')
+                temperature = pageData.find(class_="today_nowcard-temp").span.get_text().replace("°","")
+                # data = pageData.find(id="APP").find_all('div')[0]
+                print(temperature)
+        else:
+            print("Get data from api")
     else:
         print("Zip needs to be a valid 5 digit code")
         return({"error":"Zip needs to be a valid 5 digit code"})
