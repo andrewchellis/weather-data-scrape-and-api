34802604e74b43b58b36c520bf7b0804f1488d2d
Andrew Chellis <andrewchellis@users.noreply.github.com>
2018-01-04 21:02:09 +0900

    Implement full dark sky request

--- index.py
+++ index.py
@@ -2,16 +2,20 @@
 
 import argparse
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
+baseScrapeUrl = "https://weather.com/weather/today/l/{zipcode}:4:US"
+darkSkyApiUrl = "https://api.darksky.net/forecast/{key}/{lat},{lng}"
 
-baseScrapeUrl = "https://weather.com/weather/today/l/{}:4:US"
-darkSkyKey = ""
+#Optimally needs a cache to limit api calls. Maybe thats something that my calling func should be doing?
+
 try:
     with open("darkSkyKey.txt") as darkSkyKeyFile:
         darkSkyKey = darkSkyKeyFile.read()
+    zipcodeConversion = pd.read_csv("USZipCodeToLatLong.csv",dtype=str)
 except IOError as e: 
-    raise Exception("Must supply an api key under darkSkyKey.txt") from e
+    raise Exception("Must supply an api key under darkSkyKey.txt and a csv file called USZipCodeToLatLong.csv") from e
 except:
     raise Exception("Something terrible happened")
 
@@ -19,7 +23,7 @@ def getWeatherInfo(zipcode, shouldScrape=False):
     validZip = type(zipcode)==str and len(zipcode)==5 and zipcode.isdigit()
     if validZip:
         if shouldScrape:
-            requestUrl = baseScrapeUrl.format(zipcode)
+            requestUrl = baseScrapeUrl.format(zipcode=zipcode)
             weather = requests.get(requestUrl)
             if weather.status_code==200:
                 pageData = BeautifulSoup(weather.text, 'html.parser')
@@ -27,7 +31,18 @@ def getWeatherInfo(zipcode, shouldScrape=False):
                 # data = pageData.find(id="APP").find_all('div')[0]
                 print(temperature)
         else:
+            try:
+                latAndLong = zipcodeConversion.loc[zipcodeConversion['ZIP']==zipcode].iloc[0]
+            except:
+                print("Zip mapping missing for"+zipcode)
+                return({"error":"Zip mapping missing for"+zipcode})
+            print(latAndLong)
+            requestUrl = darkSkyApiUrl.format(key=darkSkyKey,lat=latAndLong["LAT"],lng=latAndLong["LNG"])
+            print(requestUrl)
+            weatherData = requests.get(requestUrl)
+            print(weatherData.json())
             print("Get data from api")
+            #Still needs to return weather data
     else:
         print("Zip needs to be a valid 5 digit code")
         return({"error":"Zip needs to be a valid 5 digit code"})
